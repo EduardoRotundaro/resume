53090817b4b8ea44d232d84bc7d671e7b91e2147
Eduardo Henrique Rotundaro <eduardo.rotundaro@olhonocarro.com.br>
2020-03-23 20:17:14 -0300

    :tada: : First commit

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React, {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <StrictMode>
+        <div>(y)</div>
+    </StrictMode>,
+    document.getElementById('root')
+);
