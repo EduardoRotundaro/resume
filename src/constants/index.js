c6d657ecae729109ec70786ce6e355ee68f5024b
Eduardo Henrique Rotundaro <eduardo.rotundaro@olhonocarro.com.br>
2020-03-23 23:41:30 -0300

    :sparkles: : Adding CV info

---
+++ src/constants/index.js
@@ -0,0 +1,89 @@
+export const CV = {
+    NAME: 'Eduardo Henrique de F. Rotundaro',
+    TITLE: 'Desenvolvedor frontend',
+    ABOUT: '24 anos, de Minas Gerais. Engenheiro de computação, formado no Instituto Nacional de Telecomunicações. Trabalho com aplicações backend e banco de dados, mas tenho maior foco em aplicações frontend. Atualmente sou desenvolvedor fullstack de aplicações web, na empresa Olho no Carro.',
+    CONTACT: [
+        { name: 'Santa Rita do Sapucaí, MG.', icon: 'home' },
+        { name: '(35) 98833-3510', icon: 'phone-square' },
+        { name: 'eduardorotundaro@gmail.com', ref: 'mailto:eduardorotundaro@gmail.com', icon: 'envelope' },
+        { name: 'github.com/EduardoRotundaro', ref: 'https://github.com/EduardoRotundaro', icon: 'github' },
+        { name: 'linkedin.com/in/eduardo-rotundaro', ref: 'https://www.linkedin.com/in/eduardo-rotundaro/', icon: 'linkedin-square' },
+    ],
+    INFO: [
+        { name: 'Objetivo', data: 'Desenvolvedor frontend.' },
+        { name: 'Tipo de contrato', data: 'PJ ou CLT.' },
+    ],
+    SKILLS: [
+        { name: 'Desenvolvimento Web', type: 'sector' },
+        { name: 'UI Design', type: 'sector' },
+        { name: 'Scrum', type: 'sector' },
+        { name: 'Testes de Software', type: 'sector' },
+        { name: 'Javascript', type: 'tech' },
+        { name: 'HTML', type: 'tech' },
+        { name: 'CSS', type: 'tech' },
+        { name: 'SASS', type: 'tech' },
+        { name: 'React.js', type: 'tech' },
+        { name: 'Redux', type: 'tech' },
+        { name: 'Git', type: 'tech' },
+        { name: 'Node.js', type: 'tech' },
+        { name: 'AngularJS', type: 'tech' },
+        { name: 'Express.js', type: 'tech' },
+        { name: 'Koa.js', type: 'tech' },
+        { name: 'MongoDB', type: 'tech' },
+        { name: 'SQL', type: 'tech' },
+    ],
+    LANGUAGE: [
+        { name: 'Inglês', level: 'Nível intermediário.' },
+    ],
+    EDUCATION: [
+        { 
+            name: 'Bacharelado em Engenharia de Computação',  
+            school: 'Instituto Nacional de Telecomunicações - INATEL',
+            date: 'Agosto/2014 - Julho/2019',
+        }
+    ],
+    EXPERIENCE: [
+        {
+            name: 'Desenvolvedor full-stack',
+            company: 'Olho no Carro',
+            date: 'Agosto/2019 - O momento',
+            description: 'Principais atividades: desenvolvimento de aplicações backend e frontend.\nTecnologias: AngularJS, React, PHP, Node.js, MongoDB, Git.'
+        },
+        {
+            name: 'Estagiário em desenvolvimento web',
+            company: 'Olho no Carro',
+            date: 'Agosto/2018 - Julho/2019',
+            description: 'Principais atividades: desenvolvimento de aplicações backend e frontend.\nTecnologias: AngularJS, React, PHP, Node.js, MongoDB, Git.'
+        },
+        {
+            name: 'Estagiário em QA',
+            company: 'Laboratório QTT - Inatel Competence Center / Qualcomm',
+            date: 'Maio/2017 - Julho/2018',
+            description: 'Principais atividades: testes de aplicativos móveis e aplicações desktop, desenvolvimento de scripts para testes automatizados, correções e desenvolvimento de pequenas implementações em projetos.\nTecnologias: Java, Python, Git.'
+        },
+    ],
+    ACTIVITIES: [
+        {
+            name: 'Maratonas de programação',
+            description: 'Ex-integrante da equipe de maratonas de programação do Inatel.'
+        },
+        {
+            name: 'Educador voluntário',
+            entity: 'Projeto Casa Viva - Inatel',
+            date: 'Julho/2017 - Dezembro/2017',
+            description: 'Conteúdo ministrado: aulas de microinformática básica, treinamentos em ferramentas como Microsoft Office e LibreOffice.',
+        },
+        {
+            name: 'Educador voluntário',
+            entity: 'Projeto Casa Viva - Inatel',
+            date: 'Janeiro/2017 - Junho/2017',
+            description: 'Conteúdo ministrado: aulas de programação básica, lógica, conceitos e estruturas de programação, desenvolvimento básico na linguagem C++.',
+        },
+        {
+            name: 'Participação na 36ª feira tecnológica do Inatel',
+            entity: 'Projeto desenvolvido: Aplicativo Safe Street',
+            date: '2017',
+            description: 'Descrição: consiste em um aplicativo desenvolvido para a plataforma Android, no qual os usuários podem cadastrar ocorrências do tipo criminalidade ou gestão pública sobre determinado local, que serão direcionadas aos órgãos responsáveis. O aplicativo também permite que o usuário acesse essas informações que foram cadastradas por outros usuários.',
+        },
+    ],
+}
