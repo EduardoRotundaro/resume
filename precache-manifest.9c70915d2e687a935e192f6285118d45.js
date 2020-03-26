8d202b9f09af987a8e62e6fd7c8df1511076f090
Eduardo Henrique Rotundaro <eduardo.rotundaro@olhonocarro.com.br>
2020-03-25 23:15:09 -0300

    Updates

---
+++ precache-manifest.9c70915d2e687a935e192f6285118d45.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6d6a00449c9624c0b35d344f72ba93",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "291e6180827649844d16",
+    "url": "/resume/static/css/main.fc451386.chunk.css"
+  },
+  {
+    "revision": "e75080deef35b62e7a67",
+    "url": "/resume/static/js/2.d5610c69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.d5610c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "291e6180827649844d16",
+    "url": "/resume/static/js/main.14dbb5ca.chunk.js"
+  },
+  {
+    "revision": "bcaa68c0b5f7adc089f7",
+    "url": "/resume/static/js/runtime-main.05fbbc33.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/resume/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/resume/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/resume/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/resume/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/resume/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "787fd82021805ebe20c17d0ce99c40ed",
+    "url": "/resume/static/media/profile.787fd820.png"
+  }
+]);
